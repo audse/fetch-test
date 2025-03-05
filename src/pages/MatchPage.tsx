ec2c4864a01b4a2f88696dd707291c53464574df
Audrey Serene <audrey.serene@gmail.com>
2025-03-04 19:38:30 -0600

    Add text to match page

--- src/pages/MatchPage.tsx
+++ src/pages/MatchPage.tsx
@@ -22,6 +22,7 @@ export default function MatchPage() {
             <main className="bg-sky-600 w-screen h-screen">
                 <Container className="py-24 flex flex-col gap-4 justify-center items-center">
                     <h1 className="text-2xl font-bold text-white">It's a match!</h1>
+                    <p className="text-sky-200 text-center mb-2">You’ve found your perfect pup! This dog is ready to bring <br />joy, love, and endless tail wags into your life.</p>
                     { dog && <>
                         <img src={dog.img} alt={dog.name} className="w-full md:w-1/2 max-h-[80vh] object-cover rounded-xl" />
                         <p className="text-xl text-white mb-8">{dog.name}</p>
